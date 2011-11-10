935b1053ff74e1d8d69d3f1259f410247c939e33
digitalizARTE <digitalizarte@gmail.com>
2011-11-10 02:11:12 -0300

    Fix System.Linq

--- library/Library/CSObject.cs
+++ library/Library/CSObject.cs
@@ -28,6 +28,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.Serialization;
